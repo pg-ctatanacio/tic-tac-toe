e6ebd9fcfb982742bb0c2845a64a9c0ce4f852f6
Cris Atanacio <crisatanacio.biz@gmail.com>
2024-02-12 21:29:36 +0800

    Refactor and Add move history

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Board from "../Board/Board";
+import "./Game.css";
+
+const Game = () => {
+	const [history, setHistory] = useState<(string | null)[][]>([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState<number>(0);
+	const currentSquares = history[currentMove];
+	const xIsNext = currentMove % 2 === 0;
+
+	const handlePlay = (nextSquares: (string | null)[]) => {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
+	};
+
+	const jumpTo = (nextMove: number) => {
+		setCurrentMove(nextMove);
+	};
+
+	const moves = history.map((squares, move) => {
+		let description;
+		if (move == 0 && move != currentMove) {
+			description = "Go to game start";
+		} else if (move > 0 && move != currentMove) {
+			description = "Go to move #" + move;
+		} else {
+			description = "You are at move #" + move;
+		}
+
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{description}</button>
+			</li>
+		);
+	});
+
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+			</div>
+			<div className="game-info">
+				<ol>{moves}</ol>
+			</div>
+		</div>
+	);
+};
+
+export default Game;
